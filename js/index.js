e575a24a0fc83b22024f4949e63351ce6d5643f9
wangqiankun0201 <2353327663@qq.com>
2022-07-27 12:50:14 +0800

    end

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-07-24 20:02:52
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-07-27 01:21:32
+ * @LastEditTime: 2022-07-27 11:57:13
  * @FilePath: \毕业设计c:\Users\王乾坤\Desktop\商优选\js\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -428,6 +428,27 @@ window.onload = function () {
         var divs = document.querySelectorAll("#wrapper #content .contentMain .goodsDetailWrap .rightDetail .bottomDetail .tabContents div");
         tabfn(lis,divs);
 
+    }
+    // 右边侧边栏的点击效果
+    rightAsideBind();
+    function rightAsideBind(){
+        //找到按钮元素发生点击事件
+        var btn = document.querySelector('#wrapper .rightAside .btns');
+        // 记录初始状态
+        var flag = true;
+        // 绑定点击事件
+        btn.onclick = function(){
+            if(flag){
+                flag = false;
+                btn.className = 'btns btnsOpen';
+                btn.parentNode.className = 'rightAside asideOpen';
+            }else{
+                flag = true;
+                btn.className = 'btns btnsClose';
+                btn.parentNode.className = 'rightAside asideClose';
+            }
+
+        }
     }
 
 }
